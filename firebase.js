fc2efde0eae8f6ae3462fc0aa02787413dc5b2c2
Adhvaith Ravi <adhvaithr@adhvaiths-mbp.lan>
2024-08-04 14:27:10 -0700

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCP1gq7u7CuOu3YhlhN3Q3twvq-by86nvs",
+  authDomain: "pantry-tracker-df6e2.firebaseapp.com",
+  projectId: "pantry-tracker-df6e2",
+  storageBucket: "pantry-tracker-df6e2.appspot.com",
+  messagingSenderId: "223646985607",
+  appId: "1:223646985607:web:932a36c4c3be334ea13722",
+  measurementId: "G-1XTK6Q64SH",
+};
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
