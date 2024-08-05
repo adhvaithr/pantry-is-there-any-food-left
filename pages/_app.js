ae794de65abcb459e4f9917ff51c315be203ceda
Adhvaith Ravi <adhvaithr@adhvaiths-mbp.lan>
2024-08-04 21:14:29 -0700

    resolved blank page error

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,6 @@ import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
