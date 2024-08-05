10fda8ff3cecce1070fa2070fddac548dea54d0e
Adhvaith Ravi <adhvaithr@adhvaiths-mbp.lan>
2024-08-04 19:41:32 -0700

    resolved client component issues part 2

--- pages/layout.js
+++ pages/layout.js
@@ -3,11 +3,6 @@ import "../styles/global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Pantry Management App",
-  description: "An app to manage your pantry inventory",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
